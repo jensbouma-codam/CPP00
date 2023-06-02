7279c045944ec0fdc5383f07b9506252f7169518
Jens Bouma <account@jensbouma.nl>
2023-06-02 20:09:54 +0200

    The changes made in this commit include: - Modified megaphone.cpp to fix a bug that caused the default message to be printed even when arguments were passed to the program. - Modified megaphone.cpp to print each argument in uppercase followed by a space character. - Removed unnecessary code that printed an extra space character at the end of the output.

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -6,21 +6,16 @@ int main(int argc, char **argv)
 	std::string default_msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 	if (argc == 1)
+		std::cout << default_msg;
+	else
 	{
-		std::cout << default_msg << std::endl;
-		return EXIT_SUCCESS;
-	}
-
-	for (int i = 1; i < argc; i++)
-	{
-		const char *arg = argv[i];
-		while (*arg)
+		for (int i = 1; i < argc; i++)
 		{
-			std::cout << (char)toupper(*arg);
-			arg++;
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+			std::cout << " ";
 		}
-		std::cout << " ";
 	}
 	std::cout << std::endl;
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
